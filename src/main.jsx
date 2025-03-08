23030859a30406db16378d6a08e4310769648ed1
maksdevop <maksmm.sirotin@mail.ru>
2025-03-08 16:33:56 +0300

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { StrictMode } from 'react';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/BlogReact">
         <App />
       </BrowserRouter>
     </Provider>
